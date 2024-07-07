877f0c1c42e775ee31d22827f95a11fe2fe26822
0x6c6d <lucas_menke@icloud.com>
2024-07-07 22:59:38 +0200

    > snake

--- snake/snake.c
+++ snake/snake.c
@@ -1,47 +1,59 @@
 // gcc main.c -lSDL2 -o ../bin/snake
 
 #include "snake.h"
-#include <ios>
 
-void change_direction(snake_t *snake, direction_e dir){
-  switch(dir){
-    case RIGHT:
-      snake->dir.right = true;
-      snake->dir.left = false;
-      snake->dir.up = false;
-      snake->dir.down = false;
-    case LEFT:
-      snake->dir.right = false;
-      snake->dir.left = true;
-      snake->dir.up = false;
-      snake->dir.down = false;
-    case UP:
-      snake->dir.right = false;
-      snake->dir.left = false;
-      snake->dir.up = true;
-      snake->dir.down = false;
-    case DOWN:
-      snake->dir.right = false;
-      snake->dir.left = false;
-      snake->dir.up = false;
-      snake->dir.down = true;
+void change_direction(snake_t *snake, direction_e dir) {
+  direction_e cur_dir = (*snake).dir;
+
+  if (cur_dir == dir)
+    return;
+  if (cur_dir == UP && dir == DOWN)
+    return;
+  if (cur_dir == DOWN && dir == UP)
+    return;
+  if (cur_dir == RIGHT && dir == LEFT)
+    return;
+  if (cur_dir == LEFT && dir == RIGHT)
+    return;
+
+  snake->dir = dir;
+}
+
+void move_snake(snake_t *snake) {
+  switch ((*snake).dir) {
+  case RIGHT:
+    snake->rect.x += 1;
+    break;
+  case LEFT:
+    snake->rect.x -= 1;
+    break;
+  case UP:
+    snake->rect.y -= 1;
+    break;
+  case DOWN:
+    snake->rect.y += 1;
+    break;
   }
-};
+}
 
 int main() {
   SDL_Init(SDL_INIT_EVERYTHING);
-  SDL_Window *window = SDL_CreateWindow("Snake", SDL_WINDOWPOS_CENTERED,
-                                      SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+  SDL_Window *window = SDL_CreateWindow(
+      "Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIN_W, WIN_H, 0);
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
   SDL_Event e;
 
+  // Setup snake
   snake_t snake;
-  snake.dir.up = true;
+  snake.dir = UP;
+  snake.rect.h = SNAKE_BODY;
+  snake.rect.w = SNAKE_BODY;
+  snake.rect.x = WIN_W / 2;
+  snake.rect.y = WIN_H / 2;
 
   // Game Loop
   bool running = true;
   while (running) {
-
     // Check for user inputs
     while (SDL_PollEvent(&e)) {
       if (e.type == SDL_QUIT) {
@@ -67,12 +79,13 @@ int main() {
     }
 
     // Move snake
+    move_snake(&snake);
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderFillRect(renderer, &snake);
+    SDL_RenderFillRect(renderer, &snake.rect);
 
     SDL_RenderPresent(renderer);
     SDL_Delay(25);
--- snake/snake.h
+++ snake/snake.h
@@ -8,20 +8,18 @@
 #include <SDL2/SDL_video.h>
 #include <stdbool.h>
 
+#define WIN_H 480
+#define WIN_W 640
+#define SNAKE_BODY 10
+
 typedef enum { RIGHT, LEFT, UP, DOWN } direction_e;
 
-typedef struct _direction {
-  bool right;
-  bool left;
-  bool up;
-  bool down;
-} direction_t;
-
 typedef struct _snake {
-  direction_t dir;
-  int x, y;
+  SDL_Rect rect;
+  direction_e dir;
 } snake_t;
 
 void change_direction(snake_t *, direction_e);
+void move_snake(snake_t *);
 
 #endif
