bf50d12a7d88f04d592aee8a9d3a265bd5311589
0x6c6d <lucas_menke@icloud.com>
2024-07-08 23:14:59 +0200

    > snake

--- snake/snake.c
+++ snake/snake.c
@@ -19,45 +19,67 @@ void change_direction(snake_t *snake, direction_e dir) {
   snake->dir = dir;
 }
 
-void move_snake(snake_t *snake) {
+void move_snake(snake_t *snake, int speed) {
   switch ((*snake).dir) {
   case RIGHT:
-    snake->rect.x += 1;
+    snake->rect.x += speed;
+    if (snake->rect.x == WIN_W) {
+      snake->rect.x = 0;
+    }
     break;
   case LEFT:
-    snake->rect.x -= 1;
+    snake->rect.x -= speed;
+    if (snake->rect.x == 0) {
+      snake->rect.x = WIN_W;
+    }
     break;
   case UP:
-    snake->rect.y -= 1;
+    snake->rect.y -= speed;
+    if (snake->rect.y == 0) {
+      snake->rect.y = WIN_H;
+    }
     break;
   case DOWN:
-    snake->rect.y += 1;
+    snake->rect.y += speed;
+    if (snake->rect.y == WIN_H) {
+      snake->rect.y = 0;
+    }
     break;
   }
 }
 
+SDL_Rect create_apple() {
+  int x = REC_SIZE + rand() % (WIN_W - REC_SIZE - REC_SIZE + 1);
+  int y = REC_SIZE + rand() % (WIN_H - REC_SIZE - REC_SIZE);
+  SDL_Rect apple = {x, y, REC_SIZE, REC_SIZE};
+  return apple;
+}
+
 int main() {
+  srand(time(NULL));
+
+  // Setup variables
+  int speed = START_SPEED;
+  bool game_running = true;
+  bool new_apple = false;
+
+  // Setup snake + apple
+  SDL_Rect apple = create_apple();
+  snake_t snake = {UP, {WIN_W / 2, WIN_H / 2, REC_SIZE, REC_SIZE}};
+
+  // Setup SDL
   SDL_Init(SDL_INIT_EVERYTHING);
   SDL_Window *window = SDL_CreateWindow(
       "Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIN_W, WIN_H, 0);
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
   SDL_Event e;
 
-  // Setup snake
-  snake_t snake;
-  snake.dir = UP;
-  snake.rect.h = SNAKE_BODY;
-  snake.rect.w = SNAKE_BODY;
-  snake.rect.x = WIN_W / 2;
-  snake.rect.y = WIN_H / 2;
-
   // Game Loop
-  bool running = true;
-  while (running) {
+  while (game_running) {
     // Check for user inputs
     while (SDL_PollEvent(&e)) {
       if (e.type == SDL_QUIT) {
-        running = false;
+        game_running = false;
       } else if (e.type == SDL_KEYDOWN) {
         switch (e.key.keysym.sym) {
         case SDLK_RIGHT:
@@ -78,15 +100,27 @@ int main() {
       }
     }
 
-    // Move snake
-    move_snake(&snake);
+    // Create apple
+    if (new_apple) {
+      apple = create_apple();
+    }
 
+    // Move snake
+    move_snake(&snake, speed);
+
+    // Render map
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    // Render apple
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &apple);
+
+    // Render snake
+    SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
     SDL_RenderFillRect(renderer, &snake.rect);
 
+    // Render scene
     SDL_RenderPresent(renderer);
     SDL_Delay(25);
   }
--- snake/snake.h
+++ snake/snake.h
@@ -7,19 +7,22 @@
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_video.h>
 #include <stdbool.h>
+#include <time.h>
 
 #define WIN_H 480
 #define WIN_W 640
-#define SNAKE_BODY 10
+#define REC_SIZE 10
+#define START_SPEED 2
 
 typedef enum { RIGHT, LEFT, UP, DOWN } direction_e;
 
 typedef struct _snake {
-  SDL_Rect rect;
   direction_e dir;
+  SDL_Rect rect;
 } snake_t;
 
 void change_direction(snake_t *, direction_e);
-void move_snake(snake_t *);
+void move_snake(snake_t *, int);
+SDL_Rect create_apple();
 
 #endif
